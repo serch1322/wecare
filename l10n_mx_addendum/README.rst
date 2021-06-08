80e3a497e34caf7c7cfa901eaeffd76558bc0246
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-06-08 08:44:20 -0500

    Solo Addenda Enterprise

--- l10n_mx_addendum/README.rst
+++
@@ -1,4 +0,0 @@
-L10N MX Addendum
-================
-El módulo extiende la funcionalidad de la contabilidad de Odoo para los requerimientos en México.
-Creación de addendas.
--- l10n_mx_addendum/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import account_move_addendum_field_value
-from . import account_move_addendum_field
-from . import account_move_addendum_wizard
-from . import account_move_addendum
-from . import account_move
-from . import res_partner
